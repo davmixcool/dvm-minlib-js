e04213c165d06b12c931d5ddf536f182c516c0e9
David Oti <davmixcool@gmail.com>
2018-07-19 18:01:28 +0100

    Add mathjs lib and gitignore

---
+++ src/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+	mulNumbers: function (a,b){
+	return math.chain(a).multiply(b).value;
+	},
+
+	addNumbers: function (a,b){
+	return a+b;
+	},
+
+	subNumbers: function (a,b){
+	return a-b;
+	}
+
+}
